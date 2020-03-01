9b663ae7c4dae7a0fd4d66202956c31c71d9e9b1
gigaika <0550_ketola@hotmail.fi>
2020-03-01 19:39:57 +0200

    work on software timers

--- test/Kernel/test_os_timers.c
+++ test/Kernel/test_os_timers.c
@@ -11,6 +11,13 @@
 static void idleFn(void *ptr) {}
 static void testFn(void *ptr) {}
 
+uint32_t called = 0;
+
+static void pendSVStub(int NumCalls) {
+    called++;
+    OS_Schedule();
+}
+
 void setUp(void) {
     DisableInterrupts_Ignore();
     BSP_SysClockConfig_Ignore();
@@ -24,4 +31,27 @@ void setUp(void) {
 
 void tearDown(void) {
     OS_ResetState();
+}
+
+void periodicThreadGetsScheduled(void) {
+    BSP_TriggerPendSV_AddCallback(&pendSVStub);
+
+    StackElementTypeDef testStack1[20];
+    OS_CreateThread(&testFn, testStack1, 20, 3, "test thread1");
+    StackElementTypeDef testStack1[20];
+    OS_CreatePeriodicThread(&testFn, testStack1, 20, 1, "test thread2", 10*SYS_TICK_PERIOD_MILLIS);
+
+    OS_SemaphoreObjectTypeDef testSemaphore1;
+    OS_InitSemaphore(&testSemaphore1, SEMAPHORE_FLAG);
+
+    runPtr = OS_GetReadyThreadByIdentifier("test thread2");
+    // OS wait would be called in beginning of thread main loop, do it here instead for simplicity
+    OS_Wait(&testSemaphore1);
+
+    for (i = 0; i < 10; i++) {
+        SysTickHandler();
+        EXPECT_EQUAL_PTR(runPtr, OS_GetReadyThreadByIdentifier("test thread1"));
+    }
+
+    EXPECT_EQUAL_PTR(runPtr, OS_GetReadyThreadByIdentifier("test thread2"));
 }
